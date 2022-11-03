f53010b08ac39bf415c35d8a522231b1b6107bee
Jasmeet Singh Chawla <60892009+jasi381@users.noreply.github.com>
2022-11-03 12:13:03 +0530

    .

---
+++ app/src/main/java/com/example/customapplication/TransitionActivity.kt
@@ -0,0 +1,40 @@
+package com.example.customapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.customapplication.databinding.ActivityTransitionBinding
+
+class TransitionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTransitionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTransitionBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btn1.primaryButtonLongNeutral()
+
+        //post delay of 5 sec after button is clicked
+
+        binding.btn1.setOnClickListener {
+            binding.btn1.primaryButtonLongClicked()
+
+
+            binding.btn1.postDelayed({
+                binding.btn2.visibility = View.VISIBLE
+                binding.btn1.visibility = View.GONE
+            }, 800)
+
+            binding.btn1.postDelayed({
+                binding.btn1.visibility = android.view.View.VISIBLE
+                binding.btn1.primaryButtonLongDisabled()
+                binding.btn2.visibility= android.view.View.GONE
+            }, 2000)
+        }
+
+    }
+}
